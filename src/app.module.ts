6fe500fba95bf8726b319f45ed7415fe3ad0d648
Julyemerson Leonizio <jb.leonizio@gmail.com>
2022-06-01 01:17:24 -0300

    adding docs to api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, HealthModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 
@@ -12,7 +13,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   app.useGlobalInterceptors(new DatabaseInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('IMC')
+    .setDescription('Api para cálculo do índice de massa corporal (IMC)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
