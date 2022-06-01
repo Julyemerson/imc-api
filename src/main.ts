0cafe114ad9664550f41398d172e6bb3b7346ca4
Julyemerson Leonizio <jb.leonizio@gmail.com>
2022-06-01 18:54:53 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
